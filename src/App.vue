6839ef89d38d9bbd2beb5a6f3c128f312f535de5
jason.jhang <jason.jhang@shoplineapp.com>
2021-09-05 18:45:49 +0800

    finish base layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
+    <header-bar />
     <router-view />
   </div>
 </template>
+
+<script>
+import HeaderBar from "@/components/Header";
+
+export default {
+  name: "App",
+  components: {
+    HeaderBar,
+  },
+};
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <router-link to="/" class="header-home-icon">
+      <img src="@/assets/home-page.png" />
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss">
+.header {
+  position: relative;
+  min-height: 80px;
+
+  &-home-icon {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>
