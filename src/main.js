e1df6a3c95a16a2fbba436a8b38cf83e3b3506bf
jason.jhang <jason.jhang@shoplineapp.com>
2021-09-05 18:41:21 +0800

    add base layout and amend router routes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@/scss/reset.scss";
+import "@/scss/global.scss";
 
 Vue.config.productionTip = false;
 
---
+++ src/views/Episode.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="episode"></div>
+</template>
